ee7f9d37b820aaaee880117b9d33a0ed4caf1c29
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2025-07-29 19:15:11 -0400

    partitions dumpInputFile()
    NOTES
    relocates the dumpInputFile() function into its designated source
    file, this way we can call it from elsewhere not just from the
    test source file.

--- tools/index.js
+++ tools/index.js
@@ -54,7 +54,39 @@ async function chat(host, port, data) {
 	console.log(msg);
 }
 
-module.exports = { models, chat, tools };
+// dumps lammps input file given the Temperature T in Kelvins and the Pressure P in bar
+async function dumpInputFile(T, P) {
+	const input = `
+include "system.in.init"
+read_data "system.data"
+include "system.in.settings"
+thermo 100
+minimize 1.0e-5 1.0e-7 1000 10000
+write_data system_minimized.data
+include "system.in.constraints"
+timestep        1 #fs
+reset_timestep 0
+thermo          1000
+thermo_style    custom step time temp pe ke etotal enthalpy press lx vol density
+velocity   all create ${T} 097865 dist gaussian
+fix 1 all npt temp ${T} ${T} 100 iso ${P} ${P} 1000.0
+run 1000000 # 1 ns
+write_data system_npt_equil.data
+unfix 1
+fix 2 all nvt temp ${T} ${T} 100
+unfix 2
+dump 3 all dcd 1000 trajectory.dcd
+run 500000 # 0.5 ns
+write_data system_nvt_equil.data
+`;
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(input);
+		});
+	});
+}
+
+module.exports = { models, chat, dumpInputFile, tools };
 
 /*
 
